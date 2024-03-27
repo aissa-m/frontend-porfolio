90b088776abfffa313583d27f5c9df370623615e
Aissa <issamaymoutt17@gmail.com>
2024-03-27 23:30:51 +0100

    Cambios en la presentación

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './homePage.css';
-import profile from '../../img/tutor-tools.png'; // Cambia esto por tu imagen
+import profile from '../../img/tutor-tools.png'; // Asegúrate de usar tu imagen actualizada
 
 const Homepage = () => {
     return (
         <div className="homepage">
             <div className="content-section">
-                <h1>Full-Stack<span>Developer</span></h1>
-                <p className="subtitle">Desarrollador Web y Tutor en Línea</p>
-                <p>Desarrollo soluciones web personalizadas, contacte <a href="/contacto">aquí</a> y ofrezco clases particulares online. 
-                Expertise en Laravel, React, Typeorm, Node Js y más. 
-                <a href="https://www.tusclasesparticulares.com/online/programacion/clases-javascript-basico-avanzado-online-3255748" target="_blank" rel="noopener noreferrer">Consulta mi perfil aquí.</a></p>
-                {/* Asegúrate de que la ruta a tu CV es correcta */}
-                <a href={`${process.env.PUBLIC_URL}/cv/CV AISSA MAYMOUTT - DEV.pdf`} download="Aissa_M_CV" className="btn-download">Descargar CV</a>
+                <h1>Full-Stack <span>Developer</span> & Online Tutor</h1>
+                <p className="subtitle">Soluciones Web a la Medida & Clases de Programación Online</p>
+                <p>Como Full-Stack Developer con experiencia en Laravel, React, Typeorm y Node.js, ofrezco soluciones web que impulsan tu negocio. Paralelamente, como tutor online, te guío en el aprendizaje de la programación, desde lo básico hasta niveles avanzados. Explora cómo puedo llevarte al éxito en tu proyecto web o en tu carrera como desarrollador. </p>
+                <p>¿Listo para empezar? <a href="/contacto">Hablemos</a>. O si prefieres, <a href="https://www.tusclasesparticulares.com/online/programacion/clases-javascript-basico-avanzado-online-3255748" target="_blank" rel="noopener noreferrer">conoce más sobre mis clases aquí.</a></p>
+                <a href={`${process.env.PUBLIC_URL}/cv/CV AISSA MAYMOUTT - DEV.pdf`} download="Aissa_M_CV" className="btn-download">Descarga Mi CV</a>
             </div>
             <div className="profile-section">
-                <img src={profile} alt="Aissa M" className="profile-image"/>
+                <img src={profile} alt="Perfil de Aissa M" className="profile-image"/>
             </div>
         </div>
     );
